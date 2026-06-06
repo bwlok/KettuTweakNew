d70e44e3099cd48965ebcf95d325d9924ebe8ea2
bwlok <bwlok.t@gmail.com>
2026-06-06 15:48:02 +0200

    feat: log to files

--- Headers/Logger.h
+++ Headers/Logger.h
@@ -1,4 +1,10 @@
 #import <Foundation/Foundation.h>
 
-#define LOG_PREFIX         @"[Bunny]"
-#define BunnyLog(fmt, ...) NSLog((LOG_PREFIX @" " fmt), ##__VA_ARGS__)
+#define LOG_PREFIX         @"[Kettu]"
+#define BunnyLog(fmt, ...) [Logger logWithFormat:LOG_PREFIX @" " fmt, ##__VA_ARGS__]
+#define LOG_FILE [NSString stringWithFormat:@"%@/%@", NSHomeDirectory(), @"Documents/pyoncord/kettu.log"]
+
+
+@interface Logger : NSObject
++ (void)logWithFormat:(NSString *)format, ... NS_FORMAT_FUNCTION(1,2);
+@end
--- Sources/Logger.m
+++ Sources/Logger.m
@@ -1,1 +1,53 @@
 #import "Logger.h"
+
+@implementation Logger
+
++ (void)logWithFormat:(NSString *)format, ... {
+    va_list args;
+    va_start(args, format);
+    NSString *message = [[NSString alloc] initWithFormat:format arguments:args];
+    va_end(args);
+
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss.SSS";
+    NSString *timestamp = [formatter stringFromDate:[NSDate date]];
+    NSString *logLine = [NSString stringWithFormat:@"%@ %@\n", timestamp, message];
+
+    NSLog(@"%@", logLine);
+
+    [self writeToLogFile:logLine];
+}
+
++ (void)writeToLogFile:(NSString *)logLine {
+    NSFileHandle *handle = [NSFileHandle fileHandleForWritingAtPath:LOG_FILE];
+    if (!handle) {
+        [logLine writeToFile:LOG_FILE atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    } else {
+        [handle seekToEndOfFile];
+        [handle writeData:[logLine dataUsingEncoding:NSUTF8StringEncoding]];
+        [handle closeFile];
+    }
+
+    [self trimLogFile:LOG_FILE toMaxLines:500];
+}
+
++ (void)trimLogFile:(NSString *)filePath toMaxLines:(NSUInteger)maxLines {
+    NSString *content = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+    if (!content) return;
+
+    NSArray *lines = [content componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+    NSMutableArray *nonEmptyLines = [NSMutableArray array];
+    for (NSString *line in lines) {
+        if (line.length > 0) [nonEmptyLines addObject:line];
+    }
+
+    if (nonEmptyLines.count <= maxLines) return;
+
+    NSRange range = NSMakeRange(nonEmptyLines.count - maxLines, maxLines);
+    NSArray *lastLines = [nonEmptyLines subarrayWithRange:range];
+    NSString *trimmed = [[lastLines componentsJoinedByString:@"\n"] stringByAppendingString:@"\n"];
+
+    [trimmed writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+}
+
+@end
